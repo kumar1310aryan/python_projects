8446dd88764bf3b3ce832a3ea1b7e383d05b0814
Aryan Kumar <kumar1310aryan@gmail.com>
2024-06-04 07:45:03 +0530

    Update Sentiment_analysis.py

--- Sentiment Analysis/Sentiment_analysis.py
+++ Sentiment Analysis/Sentiment_analysis.py
@@ -1,12 +1,10 @@
 from textblob import TextBlob
 
 def analyze_sentiment(text):
-    # Create a TextBlob object
+    
     blob = TextBlob(text)
-    # Get the sentiment polarity
     sentiment_polarity = blob.sentiment.polarity
     
-    # Determine the sentiment category
     if sentiment_polarity > 0:
         sentiment = "Positive"
     elif sentiment_polarity < 0:
@@ -17,12 +15,9 @@ def analyze_sentiment(text):
     return sentiment, sentiment_polarity
 
 if __name__ == "__main__":
-    # Get user input
     user_text = input("Please enter the text you want to analyze the sentiment of: ")
     
-    # Analyze the sentiment of the user input
     sentiment, polarity = analyze_sentiment(user_text)
     
-    # Print the sentiment analysis result
     print(f"Detected sentiment: {sentiment}")
     print(f"Sentiment polarity: {polarity}")
